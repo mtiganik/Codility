e6b35a8508cb038e92b17bbac471dfb536430813
Mihkel Tiganik <mtiganik@gmail.com>
2023-03-01 00:42:34 +0200

    task1

--- react-js/src/ex1.js
+++ react-js/src/ex1.js
@@ -1,11 +1,19 @@
 import React from "react"
 
-function Example1(){
-    const [data,setData] = React.useState("Hello")
+export default function LoginForm({ onSubmit }) {
+    const [username, setUserame] = React.useState("");
+    const [password, setPassword] = React.useState("");
 
-    return(
-        <p>{data}</p>
-    )
-}
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onSubmit(username, password);
+        }
+    const submitEnabled = username.length > 0 && password.length > 0 
 
-export default Example1
+    return (<div>
+        <input type="text" id="username-input" onChange={(event) => setUserame(event.target.value)} value = {username} />
+        <input type="password" id="password-input" onChange={(event) => setPassword(event.target.value)} value = {password} />
+        <input type="submit" id="login-button" disabled={!submitEnabled} onClick={handleSubmit}></input>
+    </div>);
+  }
+  
